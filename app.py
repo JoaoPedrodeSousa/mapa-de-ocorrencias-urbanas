04a1bf44f91713e64244cec47244f3ce06b02a2a
João Pedro <joaopdesousa12@gmail.com>
2025-04-07 00:15:20 -0300

    feature - add migrations

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from src.infrastructure.server import server
 from src.infrastructure.database.database import connection
+from src.infrastructure.database.models import *
 
+
+app = server.get_app()
 if __name__ == "__main__":
     db = connection.get_db()
     migrate = connection.get_migrate()
-    
+
     server.init_server()
