5865f7e794ed19360ff031b7e5e6d4c692d1f4ae
João Pedro <joaopdesousa12@gmail.com>
2025-04-11 13:31:29 -0300

    update - migrations upgrade

--- src/infrastructure/database/models.py
+++ src/infrastructure/database/models.py
@@ -5,19 +5,19 @@ from .database import connection
 db = connection.get_db()
 
 class CategoryModel(db.Model):
-    __tablename__ = 'tb_categoria'
+    __tablename__ = 'categoria'
 
     id = db.Column(db.Integer, primary_key = True)
     nome = db.Column(db.String(30),nullable=False)
 
 class OccurrenceModel(db.Model):
-  __tablename__ = 'tb_ocorrencias'
+  __tablename__ = 'ocorrencias'
 
   id = db.Column(db.Integer, primary_key = True)
-  categoria_id = db.Column(db.SmallInteger, db.ForeignKey('tb_categoria.id'), nullable = False)
+  categoria_id = db.Column(db.SmallInteger, db.ForeignKey('categoria.id'), nullable = False)
   descricao = db.Column(db.String(125))
   data_registro = db.Column(db.DateTime(timezone=False),nullable = False)
-  geom = db.Column(Geometry(geometry_type='POINT', srid=31983, spatial_index=True))
+  geom = db.Column(Geometry(geometry_type='POINT', srid=4326, spatial_index=True))
 
   dom_categoria = db.relationship(
     'CategoryModel',
