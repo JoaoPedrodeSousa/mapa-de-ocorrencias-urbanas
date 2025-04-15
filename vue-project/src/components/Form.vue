c636aa2823ece25855865aae94751f86e2da8f8e
João Pedro <joaopdesousa12@gmail.com>
2025-04-15 02:33:49 -0300

    update - simplify field component

--- vue-project/src/components/Form.vue
+++ vue-project/src/components/Form.vue
@@ -3,12 +3,13 @@
     <h1>Cadastro de Ocorrências Urbanas</h1>
     <div class="form-container">
       <form @submit.prevent="sendForm">
-        <CategoryField class="input-container" name="Categoria" v-model="category"/>
-        <FormField class="input-container" type="date" name="Data de Registro" id="data" v-model="date"/>
-        <FormField class="input-container" type="text" name="Descrição do Problema" id="descricao" v-model="description" />
+        <FormField class="input-container" type="select" name="Categoria" id="categoria" v-model="category"/>
 
+        <FormField class="input-container" type="date" name="Data de Registro" id="data" v-model="date"/>
+        <FormField class="input-container" type="textarea" name="Descrição do Problema" id="descricao" v-model="description" />
         <div class="input-container">
           <label>Coordenadas</label>
+          
           <div class="coordinates-container">
             <FormField class="coordinates" type="text" name="X" id="X" v-model="x"/>
             <FormField class="coordinates" type="text" name="Y" id="Y" v-model="y"/>
@@ -22,7 +23,6 @@
 
 <script setup>
 import FormField from "./FormField.vue";
-import CategoryField from "./CategoryField.vue";
 import { ref } from "vue";
 
 const category = ref('')
@@ -32,26 +32,26 @@ const x = ref(0)
 const y = ref(0)
 
 function sendForm(){
-  
+
 }
 </script>
 
 <style scoped>
 section{
   display: grid;
+  gap: 4rem;
   align-content: center;
-  gap: 3rem;
   width: max-content;
   height: 100vh;
-  padding: 0 1.2rem;
+  padding: 0 1.5rem;
   color: #e0e0e0;
   background: linear-gradient(180deg, #121212 0%, #1E1E1E 100%);
 }
 
 h1{
-    font-size: 1.5rem;
-    text-decoration:underline 3px #1DB954;
-    text-align: center;
+  text-decoration:underline 3px #1DB954;
+  text-align: center;
+  font-size: 1.5rem;
 }
 
 form{
@@ -80,12 +80,12 @@ label{
 }
 
 button{
-  background-color: #1DB954;
-  color: #121212;
   border: 2px solid transparent; 
-  font-weight: bold; 
-  font-size: 1rem;
   padding: 0.5rem;
+  font-size: 1rem;
+  font-weight: bold;
+  color: #014218;
+  background-color: #1DB954;
 }
 
 button:hover{
--- vue-project/src/components/FormField.vue
+++ vue-project/src/components/FormField.vue
@@ -1,30 +1,65 @@
 <template>
-  <div class="input-container">
+  <div>
     <label :for="id">{{ name }}</label>
-    <input :type="type" :name="name" :id="id" :value="modelValue"  @input="$emit('update:modelValue', $event.target.value)"/>
+    <input v-if="type === 'text' || type === 'date'" 
+      :type="type" 
+      :name="name" 
+      :id="id" 
+      :value="modelValue"  
+      @input="$emit('update:modelValue', $event.target.value)"
+    />
+
+    <select v-else-if="type === 'select'" 
+      :name="name" 
+      id="category" 
+      :value="modelValue" 
+      @change="$emit('update:modelValue', $event.target.value)">
+
+      <option v-for="(category) in categories" :key="category.id" >
+        {{category.name}}
+      </option>
+
+    </select>
+
+    <textarea v-else :name="name" :id="id" :value="modelValue" @input="$emit('update:modelValue', $event.target.value)" placeholder="Forneça um resumo da ocorrência."></textarea>
   </div>
 </template>
 
 <script setup>
-import { defineProps } from "vue";
+import { defineProps, ref, onMounted } from 'vue';
+
+const categories = ref([])
 
 const props = defineProps({
   type: String,
   name: String,
   id: String,
-  src:String,
   modelValue: [String, Number]
 });
 
+onMounted(async () => {
+  const response = await fetch('http://127.0.0.1:5000/category')
+  categories.value = await response.json()
+})
 </script>
 
 <style scoped>
 label{
   font-size: 1.2rem;
 }
+input, textarea{
+  all: unset;
+}
+textarea{
+  resize: vertical;
+  overflow: hidden;
+  overflow-y:scroll;
+  overflow-wrap: break-word;
+  min-height: 60px;
+  max-height: 100px;
+}
 
-input{
-    all: unset;
+input,textarea{
     padding: 0.6rem;
     font-size: 1rem;
     border: 2px solid #2A2A2A;
@@ -32,36 +67,24 @@ input{
     color: #E0E0E0;
     box-shadow: none;
     box-sizing: border-box;
+    max-width: 100%;
 }
 
-input:hover, input:focus{
+input:hover, input:focus,
+textarea:hover, textarea:focus{
+  border: 2px solid #1DB954;
+}
+
+select {
+  padding: 0.6rem;
+  border: 2px solid #2A2A2A;
+  background-color: #181818;
+  color: #E0E0E0;
+  font-size: 1rem;
+}
+
+select:hover{
   border: 2px solid #1DB954;
 }
 
 </style>
-
-<!-- Botões:
-
-    Fundo: #1DB954
-
-    Texto: #121212
-
-    Hover: #1ED760
-
-Inputs e campos:
-
-    Fundo: #181818
-
-    Borda: #2A2A2A
-
-    Texto: #E0E0E0
-
-Links:
-
-    Normal: #1DB954
-
-    Hover: #1ED760
-
-Background geral:
-
-    Gradiente opcional: linear-gradient(180deg, #121212 0%, #1E1E1E 100%) -->
