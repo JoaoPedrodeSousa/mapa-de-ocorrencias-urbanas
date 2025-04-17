542cd79c2b3ef35ed0e827a92e65c5c4c12898a4
João Pedro <joaopdesousa12@gmail.com>
2025-04-17 12:23:46 -0300

    refactor - delete form field component and add fields in form component

--- vue-project/src/components/Form.vue
+++ vue-project/src/components/Form.vue
@@ -3,11 +3,25 @@
     <h1>Cadastro de Ocorrências Urbanas</h1>
     <div class="form-container">
       <form @submit.prevent="handleForm">
-        <FormField @change="changeCategory" class="input-container" type="select" name="Categoria" id="categoria" v-model="category"/>
+        <div class="input-container">
+          <label for="category">Categoria</label>
+          <select id="category" name="category" v-model="category">
+            <option disabled value="">Selecione uma categoria</option>
+            <option v-for="(ctg) in categories" :key="ctg.id" :value="ctg.id">
+            {{ctg.name}}
+            </option>
+          </select>
+        </div>
 
-        <FormField class="input-container" type="date" name="Data de Registro" id="data" v-model="datetime"/>
+        <div class="input-container">
+          <label for="date">Data de Registro</label>
+          <input type="date" id="date" name="date" v-model="datetime">
+        </div>
 
-        <FormField class="input-container" type="textarea" name="Descrição do Problema" id="descricao" v-model="description" />
+        <div class="input-container">
+          <label for="description">Descrição do Problema</label>
+          <textarea id="description" name="description" placeholder="Forneça um resumo da ocorrência." v-model="description"></textarea>
+        </div>
 
         <div class="input-container">
           <h2>Coordenadas</h2>
@@ -23,28 +37,23 @@
 </template>
 
 <script setup>
-import FormField from "./FormField.vue";
-import { ref, defineProps, defineEmits } from "vue";
+import { ref, defineProps, defineEmits, onMounted } from "vue";
 
+const categories = ref([])
 const category = ref('')
 const description = ref('')
 const datetime = ref(null)
+const success = ref(null)
 
 const props = defineProps({
   lng:Number,
   lat:Number,
 })
 
-const success = ref(null)
-const emit = defineEmits(['handleSuccess', 'handleCategory'])
-
-function changeCategory(){
-  emit('handleCategory', category.value)
-}
+const emit = defineEmits(['handleSuccess'])
 
 function isSuccess(){
   emit('handleSuccess', success.value)
-  success.value = null
 }
 
 async function handleForm(){
@@ -54,7 +63,7 @@ async function handleForm(){
     description: description.value,
     geometry: [props.lng,props.lat]
   }
-
+  console.log(body)
   const response = await fetch('http://localhost:5000/occurrence',{
     method: 'POST',
     headers:{
@@ -63,14 +72,24 @@ async function handleForm(){
     body: JSON.stringify(body)
     })
   
-  if(response.ok) success.value = 'true'
+  if(response.ok) {
+    success.value = 'true'
+    category.value = ''
+    description.value = ''
+    datetime.value = null
+  }
   else if (!response.ok) success.value = 'false'
   isSuccess()
 }
+
+onMounted(async () => {
+  const response = await fetch('http://127.0.0.1:5000/category')
+  categories.value = await response.json()
+})
 </script>
 
 <style scoped>
-section{
+section {
   display: grid;
   gap: 4rem;
   align-content: center;
@@ -78,32 +97,60 @@ section{
   height: 100vh;
   padding: 0 1.5rem;
   color: #e0e0e0;
-  background: linear-gradient(180deg, #121212 0%, #1E1E1E 100%);
+  background: linear-gradient(180deg, #121212, #1E1E1E);
   border-left: 3px solid #1DB954;
 }
 
-h1{
-  text-decoration:underline 3px #1DB954;
+h1 {
+  text-decoration: underline 3px #1DB954;
   text-align: center;
   font-size: 1.5rem;
 }
 
-form{
-    display: grid;
-    gap: 2.5rem;
+form {
+  display: grid;
+  gap: 2.5rem;
 }
 
-.input-container{
-    display: grid;
-    gap: 0.25rem;
+.input-container {
+  display: grid;
+  gap: 0.25rem;
 }
 
-h2{
+label, h2 {
   font-size: 1.2rem;
-  font-weight: normal;
 }
 
-.coordinates-container{
+input, textarea, select, p span {
+  all: unset;
+  padding: 0.6rem;
+  font-size: 1rem;
+  border: 2px solid #2A2A2A;
+  background-color: #181818;
+  color: #E0E0E0;
+  box-sizing: border-box;
+  max-width: 100%;
+}
+
+input:hover, input:focus,
+textarea:hover, textarea:focus,
+select:hover {
+  border-color: #1DB954;
+}
+
+textarea {
+  resize: vertical;
+  min-height: 60px;
+  max-height: 100px;
+  overflow-y: auto;
+  word-break: break-word;
+}
+
+select {
+  appearance: none;
+}
+
+.coordinates-container {
   display: grid;
   grid-template-columns: 1fr 1fr;
   gap: 1rem;
@@ -111,39 +158,27 @@ h2{
 
 .coordinates {
   display: flex;
-  place-items: center;
-}
-
-p{
-  display: grid;
-  gap: 0.5rem;
+  align-items: center;
   font-size: 1.2rem;
 }
 
-p span{
-    padding: 0.6rem;
-    border: 2px solid #2A2A2A;
-    background-color: #181818;
-    box-shadow: none;
-    box-sizing: border-box;
-    font-size: 1rem;
-    width: 10rem;
-    max-width: 100%;
-    font-style: italic;
-    color: #E0E0E0;
+p span {
+  font-style: italic;
+  width: 10rem;
 }
 
-button{
-  border: 2px solid transparent; 
+button {
   padding: 0.5rem;
   font-size: 1rem;
   font-weight: bold;
   color: #014218;
   background-color: #1DB954;
+  border: 2px solid transparent;
+  cursor: pointer;
 }
 
-button:hover{
-  background-color:#1ED760;
+button:hover {
+  background-color: #1ED760;
 }
-
 </style>
+
